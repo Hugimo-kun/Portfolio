b8db9c8aec43218cc88866781fa1186d26efe6d5
Hugimo-kun <hugo.simoneau@gmail.com>
2024-07-04 09:35:58 +0200

    JS eventListener added on the DOMContentLoaded

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
-  hamburger.classList.toggle("is-active");
-});
-function toggleAside() {
-document.querySelector("aside").classList.toggle("is-out")
-}
+document.addEventListener("DOMContentLoaded", function () {
+  let hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+  });
+  function toggleAside() {
+    document.querySelector("aside").classList.toggle("is-out");
+  }
 
-document.getElementById('popupImg').addEventListener('click', function() {
-  document.getElementById('sectionpopup').style.display = 'flex';
-  document.getElementById('body').style.overflowY = 'hidden';
-});
+  document.getElementById("popupImg").addEventListener("click", function () {
+    document.getElementById("sectionpopup").style.display = "flex";
+    document.getElementById("body").style.overflowY = "hidden";
+  });
 
-document.getElementById('close').addEventListener('click', function() {
-  document.getElementById('sectionpopup').style.display = 'none';
-  document.getElementById('body').style.overflowY = 'scroll';
+  document.getElementById("close").addEventListener("click", function () {
+    document.getElementById("sectionpopup").style.display = "none";
+    document.getElementById("body").style.overflowY = "scroll";
+  });
 });
