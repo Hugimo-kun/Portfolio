75bbf4d210b104ed9a34374133cbfca0298045b0
Hugimo-kun <hugo.simoneau@gmail.com>
2024-11-18 13:40:21 +0100

    fixed the aside is out bug

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let hamburger = document.querySelector(".hamburger");
   hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
-  });
-  function toggleAside() {
     document.querySelector("aside").classList.toggle("is-out");
-  }
+  });
 
   document.getElementById("popupImg").addEventListener("click", function () {
     document.getElementById("sectionpopup").style.display = "flex";
